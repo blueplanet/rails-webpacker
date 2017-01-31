d184eb0b011257f62c78a22e7f15088133ca6d5e
blueplanet <erguolinge@gmail.com>
2017-02-01 07:33:59 +0900

    count

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'count/index'
+
   root to: 'sample#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
